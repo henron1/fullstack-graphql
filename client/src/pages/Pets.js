4456d00632bd36cc24c5c6b4ce3df5f9cb0851fd
Henry Neal <henry.neal0@gmail.com>
2019-12-27 10:55:43 -0500

    added useMutation and updating the cache to prevent manual rerender to see new pet on screen

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -6,7 +6,7 @@ import NewPetModal from "../components/NewPetModal";
 import Loader from "../components/Loader";
 
 const ALL_PETS = gql`
-	query AllPets {
+	query allPets {
 		pets {
 			id
 			type
@@ -16,19 +16,42 @@ const ALL_PETS = gql`
 	}
 `;
 
+const ADD_PET = gql`
+	mutation CreateAPet($newPet: NewPetInput!) {
+		addPet(input: $newPet) {
+			id
+			name
+			type
+			img
+		}
+	}
+`;
+
 export default function Pets() {
 	const [modal, setModal] = useState(false);
 	const { data, loading, error } = useQuery(ALL_PETS);
+	const [createPet, newPet] = useMutation(ADD_PET, {
+		update(cache, { data: { addPet } }) {
+			const allPets = cache.readQuery({ query: ALL_PETS });
+			cache.writeQuery({
+				query: ALL_PETS,
+				data: { pets: [addPet, ...data.pets] }
+			});
+		}
+	});
 
 	const onSubmit = input => {
 		setModal(false);
+		createPet({
+			variables: { newPet: input }
+		});
 	};
 
-	if (loading) {
+	if (loading || newPet.loading) {
 		return <Loader />;
 	}
 
-	if (error) {
+	if (error || newPet.error) {
 		return <p>There was an error.</p>;
 	}
 
